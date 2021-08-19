c2afc07afde2da5709e6c76459af98eee89acccd
LUNG-CHEN HUANG <lungchen.huang@gmail.com>
2021-08-19 20:33:55 +0800

    add shebang and command-line options

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,30 @@
-// import yargs from 'yargs/yargs';
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
 import { convert } from './converter';
-
-// TODO: Accept options to generate LLVM IR in different formats. For now we assume there are no options passed in.
-// const argv = yargs(process.argv.slice(2)).parse();
-
-/**
- * Invoke `convert` with entry files
- */
-convert(process.argv.slice(2));
+// Provide command line options
+const options = {
+    'emitIR': {
+        describe: 'Emit LLVM Intermediate Representation to standard output',
+        requiresArg: false,
+        boolean: true,
+        default: true,
+        require: false
+    },
+    'emitBitcode': {
+        describe: 'Emit LLVM Bitcode to a specific file',
+        requiresArg: false,
+        string: true,
+        default: 'llvm.bc',
+        require: false
+    }
+};
+// Extract arguments given the options
+// Version refers to the version inside package.json
+const argv = yargs(process.argv.slice(2)).options(options).help().string('_').check(argv => {
+    const filePaths = argv._;
+    if (filePaths.length === 0) throw new Error('At least one file should be provided.');
+    return true;
+}).parseSync();
+// convert a list of input files to IR or Bitcode file
+convert(argv._ as string[], argv.emitIR, argv.emitBitcode);
 
